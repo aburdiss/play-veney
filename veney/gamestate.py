1e91c1045836ca9cec466164a3330c50a708cf83
cdleong <4109253+cdleong@users.noreply.github.com>
2024-03-18 22:21:43 -0400

    CDL: starting on gamestate functionality. Can save/load basic stuff and print board to cli

---
+++ veney/gamestate.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+import json
+
+
+class GameState:
+
+    def __init__(self) -> None:
+        self.history = ""  # no history
+        # board is a list of lists of strings. Each list can 
+        # vertical configuration, 16 rows and 4 columns, like https://veney.xyz/images/boardgray_brightlr.webp
+        self.board = [[[""] for _ in range(4)] for _ in range(16)] 
+        
+        self.discard_pile = [""]  # no pieces in the discard
+        self._notation_to_index_dict = {
+            "w": 0,
+            "x": 1,
+            "y": 2,
+            "z": 3,
+        }
+
+    def _notation_to_index(self, pos_str: str) -> int:
+        # TODO: better version, this is inelegant
+        try:
+            return self._notation_to_index_dict[pos_str.lower()]
+        except KeyError:
+            return 16 - int(pos_str) 
+
+    def set_position(self, col: str, row: str, pieces: list):
+        """e.g. if we want to set W13 to have a Sword on it, we do set_position(col='W', row='13', pieces='$')"""
+        col_index = self._notation_to_index(col)
+        row_index = self._notation_to_index(row)
+
+        print(f"row index, col index: {row_index},{col_index}")
+
+        self.board[row_index][col_index] = [piece for piece in pieces]
+
+    def save_state(self, gamestate_file: Path):
+        gamestate_dict = {
+            "history": self.history,
+            "board": self.board,
+            "discard_pile": self.discard_pile,
+        }
+
+        with open(gamestate_file, "w") as f:
+            json.dump(gamestate_dict, f)
+
+    def load_state(self, gamestate_file: Path):
+        with open(gamestate_file, "r") as f:
+            gamestate = json.load(f)
+
+            self.history = gamestate["history"]
+            self.board = gamestate["board"]
+            self.discard_pile = gamestate["discard_pile"]
+
+
+    def __repr__(self) -> str:
+        """
+        detailed print for debug purposes
+        """
+        column_width = 3
+
+        repr = f"\n\nGAME STATE\nhistory: {self.history}\n"
+        repr = repr + "board: \n"
+        
+        print("W")
+        for i, row in enumerate(self.board):
+            repr  = repr + f"{16-i:<{column_width}}"
+            for col in row:
+                pieces = col[0]
+                repr = repr + f"|{str(pieces):^{column_width}}"
+            repr = repr +"|"+"\n"
+            repr= repr+"-"*5*(column_width+1) +"\n"
+
+
+        for letter in " wxyz":
+            repr = repr + f"{letter:^{column_width+1}}"
+        repr = repr +"\n"
+        return repr
+
+
+if __name__ == "__main__":
+    test_file = "test_file.json"
+    gamestate = GameState()
+    print(f"History at start is {gamestate.history}")
+    
+    gamestate.history = "aw,"
+
+    print(f"History after first meta-move is {gamestate.history}")
+
+    gamestate.set_position(row="4", col="X", pieces="$")
+    print(f"gamestate after first set is {gamestate}")
+    gamestate.save_state(test_file)
+    print(f"saving state at this point to {test_file}")
+
+    gamestate.history = "aw,e6"
+
+    print(f"History after first turn is {gamestate.history}")
+
+    print(f"loading state from {test_file}")
+    gamestate.load_state(test_file)
+
+    print(f"History after loading is {gamestate.history}")
+
+    print(gamestate)
